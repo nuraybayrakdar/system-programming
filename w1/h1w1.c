9f44a8f9558f39d3daf989e0912bd8efb61bb2a3
nuraybayrakdar <bayrakdarnuray@gmail.c>
2023-03-09 12:50:41 +0300

    init

---
+++ w1/h1w1.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+#include<stdlib.h>
+
+int main() {
+    //printf("Hello World\n");
+    printf("int bellek %d\n", sizeof(int));
+    printf("pointer bellek %d\n", sizeof(int *));
+    return EXIT_SUCCESS;
+}
